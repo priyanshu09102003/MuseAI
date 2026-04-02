c301dd28e0615193a447c9c30ac8e9b57201e825
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-04-03 01:06:00 +0530

    Creating an endpoint to teest model and music generation from the ACE model

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 import modal
 import os
 import uuid
+import base64
+from pydantic import BaseModel
 
 app = modal.App("music-generator")
 
@@ -21,6 +23,9 @@ model_volume = modal.Volume.from_name("ace-step-models", create_if_missing=True)
 hf_volume = modal.Volume.from_name("qw-hf-cache", create_if_missing=True)
 
 music_gen_secrets = modal.Secret.from_name("music-gen-secrets")
+
+class GenerateMusicResponse(BaseModel):
+    audio_data: str
 
 @app.cls(
     image=image,
@@ -64,14 +69,29 @@ class MusicGenServer:
         self.image_pipe.to("cuda")
 
     @modal.fastapi_endpoint(method="POST")
-    def generate(self):
+    def generate(self) -> GenerateMusicResponse:
         output_dir = "/tmp/outputs"
         os.mkdir(output_dir, exist_ok=True)
         output_path = os.path.join(output_dir, f"{uuid.uuid4()}.wav")
 
         self.music_model(
-            prompt="hip-hop, rap, new-york, joyful, 120BPM"
+            prompt="electronic rap",
+            lyrics = "[verse]\nWaves on the bass, pulsing in the speakers,\nTurn the dial up, we chasing six-figure features,\nGrinding on the beats, codes in the creases,\nDigital hustler, midnight in sneakers.\n\n[chorus]\nElectro vibes, hearts beat with the hum,\nUrban legends ride, we ain't ever numb,\nCircuits sparking live, tapping on the drum,\nLiving on the edge, never succumb.\n\n[verse]\nSynthesizers blaze, city lights a glow,\nRhythm in the haze, moving with the flow,\nSwagger on stage, energy to blow,\nFrom the blocks to the booth, you already know.\n\n[bridge]\nNight's electric, streets full of dreams,\nBass hits collective, bursting at seams,\nHustle perspective, all in the schemes,\nRise and reflective, ain't no in-betweens.\n\n[verse]\nVibin' with the crew, sync in the wire,\nGot the dance moves, fire in the attire,\nRhythm and blues, soul's our supplier,\nRun the digital zoo, higher and higher.\n\n[chorus]\nElectro vibes, hearts beat with the hum,\nUrban legends ride, we ain't ever numb,\nCircuits sparking live, tapping on the drum,\nLiving on the edge, never succumb.",
+            audio_duration=180,
+            infer_step=60,
+            guidance_scale=15,
+            save_path=output_path
         )
+
+        with open(output_path, "rb") as f:
+            audio_bytes = f.read()
+        
+        audio_b64 = base64.b64encode(audio_bytes).decode("utf-8")
+
+        os.remove(output_path)
+
+
+        return GenerateMusicResponse(audio_data=audio_b64)
 
 
 
@@ -79,7 +99,4 @@ class MusicGenServer:
 @app.local_entrypoint()
 def main():
     function_test.remote()
-
-
-
 
