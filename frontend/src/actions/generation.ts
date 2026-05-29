9c55b255e455381be9426f051fcdbdf3fb02beb1
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-05-30 00:17:23 +0530

    Fetching tracks from S3 bucket and server action to get the presigned urls and convert the files to display the tracks and thumbnails

--- frontend/src/actions/generation.ts
+++ frontend/src/actions/generation.ts
@@ -6,6 +6,8 @@ import { prisma } from "@/lib/prisma"
 import { revalidatePath } from "next/cache";
 import { headers } from "next/headers"
 import { redirect } from "next/navigation";
+import {GetObjectCommand, S3Client} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 
 export interface GenerateRequest {
@@ -62,4 +64,23 @@ export async function queueSong(generateRequest: GenerateRequest, guidanceScale:
             userId: song.userId
         }
     })
+}
+
+export async function getPresignedUrl(key: string) {
+  const s3Client = new S3Client({
+    region: process.env.AWS_REGION!,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID!  
+    }
+  });
+
+    const command = new GetObjectCommand({
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: key,
+    });
+
+    return await getSignedUrl(s3Client, command, {
+        expiresIn: 3600,
+    });
 }
--- frontend/src/app/(main)/create/page.tsx
+++ frontend/src/app/(main)/create/page.tsx
@@ -1,9 +1,11 @@
-import CreateSong from '@/custom_components/create'
+
 import { SongPanel } from '@/custom_components/createPage/song-panel'
+import TrackListFetcher from '@/custom_components/createPage/track-list-fetcher'
 import { auth } from '@/lib/auth'
+import { Loader2Icon } from 'lucide-react'
 import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
-import React from 'react'
+import { Suspense } from 'react'
 
 export default async function MainPage () {
   const session = await auth.api.getSession({
@@ -16,6 +18,9 @@ export default async function MainPage () {
   return (
     <div className='flex h-full flex-col lg:flex-row overflow-hidden'>
         <SongPanel />
+        <Suspense fallback={<div className='flex h-full w-full items-center justify-center'><Loader2Icon className='h-8 w-8 animate-spin' /></div>}>
+            <TrackListFetcher />
+        </Suspense>
     </div>
   )
 }
